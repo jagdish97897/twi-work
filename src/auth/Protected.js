33bc6dbe045f3298b605d36539950c046d582d91
nitish1899 <nitishcyberminds@gmail.com>
2024-06-24 12:55:30 +0530

    fix navigate

--- src/auth/Protected.js
+++ src/auth/Protected.js
@@ -8,7 +8,7 @@ const Protected = (props) => {
   useEffect(() => {
     let login = localStorage.getItem('token')
     if (!login) {
-      navigate('/protected')
+      navigate('/')
     }
 
   })
